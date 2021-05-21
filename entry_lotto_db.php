a2e3fb01bebec80608653f9b5f98f78f3210af6b
Shadin <shadin710@gmail.com>
2021-05-21 23:20:08 +0600

    Added initial entry for the products

---
+++ entry_lotto_db.php
@@ -0,0 +1,49 @@
+<?php
+    session_start();
+    if(empty($_SESSION['username']))
+    {
+        header("Location:index.php");
+    }
+    include_once 'connection/db_connection.php'
+    if ($_SERVER['REQUEST_METHOD']=='POST')
+    {
+        $pNAME=$_POST['pName'];
+        $category = $_POST['category'];
+        $buyPrice = $_POST['buying_price'];
+        $pDescription = $_POST['description'];
+        $uStock= $_POST['underStock'];
+        $lotto = $_POST['lotto'];
+        $dop = $_POST['dop'];
+        $expire = $_POST['Expire'];
+
+        $cName = $_POST['cName'];
+        $cEmail = $_POST['cEmail'];
+        $sName = $_POST['sName'];
+        $sAddress = $_POST['sAddress'];
+        $sCategory=  $_POST['sCategory'];
+        $sPhone = $_POST['sPhone'];
+        $sMobile = $_POST['sMobile'];
+        $sEmail = $_POST['sEmail'];
+        $ratings = $_POST['ratings']
+        $sql_stock = "INSERT INTO stock(Product_Name,category,pDescription,Under_Stock,buy_price,lotto,dop,expire,supplier_name) VALUES ('$pNAME','$category','$pDescription','$uStock','$buyPrice','$lotto','$dop','$expire','$sName')";
+
+        if(!mysqli_query($conn,$sql_article))
+        {
+            die("Failed to insert the data in stock table" . mysqli_error($conn));
+        }
+        else
+        {
+            $sql_supplier ="INSERT INTO supplier(cName,cEmail,sName,sAddress,sCategory,sEmail,sMobile,sPhone,ratings) VALUES ('$cName','$cEmail','$sName','$sAddress','$sCategory','$sEmail','$sMobile','$sPhone','$ratings')";
+
+            if (!mysqli_query($conn,$sql_supplier)) 
+            {
+                die("Failed to insert data in supplier table" . mysqli_error($conn));
+            }
+            else
+            {
+                header("Location:homepage.php");
+            }
+        }
+    }
+
+?>
