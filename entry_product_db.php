f8fbad85af652c170a5f49502c6c96436f7e6689
Shadin <shadin710@gmail.com>
2021-05-28 19:45:53 +0600

    Handled backed city table

--- entry_product_db.php
+++ entry_product_db.php
@@ -17,6 +17,7 @@
         $expire = $_POST['Expire'];
         $spp=  $_POST['spp'];
         $cost_per_room = $_POST ['cpr'];
+        $city_name = $_POST['city'];
 
         $sql_article = "INSERT INTO articles(Product_Name,category,sold_product,room,sell_price,lotto,dos,expire,Sales_Period) VALUES ('$pNAME','$category','$sell','$room_number','$sellPrice','$lotto','$dos','$expire','$spp')";
 
@@ -29,7 +30,9 @@
             $get_stock  = 'SELECT * FROM stock';
             $stock_object = mysqli_query($conn,$get_stock) Or die("Failed to query " . mysqli_error($conn));
             $stock = mysqli_fetch_assoc($stock_object);
-            $uStock = $stock['Under_Stock']- $sell;
+
+            $uStock = $stock['Under_Stock'] - $sell;
+
             $sql_stock_update = "UPDATE stock SET Under_Stock='$uStock' WHERE Product_Name='$pNAME'";
 
             if (!mysqli_query($conn,$sql_stock_update)) {
@@ -44,7 +47,27 @@
                 } 
                 else
                 {
-                    header("Location:homepage.php");
+                    //search the city and update
+
+                    $sql_find_city ="SELECT * FROM city WHERE city_name='$city_name' and Product_Name = '$pNAME'";
+                    $city_lot_object = mysqli_query($conn,$sql_find_city) Or die("failed to search city lot " . mysqli_error($conn));
+
+                    $city_object = mysqli_fetch_assoc($city_lot_object);
+
+                    //Substract the current lot with input lot
+                    $city_lotto = $city_object['city_lotto']-$lotto;
+
+                    $sql_city_update = "UPDATE city SET city_lotto = '$city_lotto' WHERE Product_Name = '$pNAME' AND city_name = '$city_name'";
+
+                    if (!mysqli_query($conn,$sql_city_update)) 
+                    {
+                        die("Failed to insert in city table" .  mysqli_error($conn));
+                    }
+                    else
+                    {
+                        header("Location:homepage.php");
+                    }
+                    
                 }
             }
             
